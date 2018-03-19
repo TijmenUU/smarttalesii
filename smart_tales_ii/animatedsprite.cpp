d5be62bbe66d5f73d3f6aeefc76db65ea3631bd1
Tijmen <tijmen_222@hotmail.com>
2018-03-19 11:28:47 +0100

    Refactored AnimatedSprite
    Split up logic into two classes, Animation::Sprite and Animation::Sheet.
    Makes copying these a whole lot simpler as well.

--- smart_tales_ii/animatedsprite.cpp
+++ smart_tales_ii/animatedsprite.cpp
@@ -1,442 +1,111 @@
 #include "animatedsprite.hpp"
-#include "platform.hpp"
-#include "stringutil.hpp"
 
-#include <array>
 #include <cassert>
-#include <sstream>
-#include <stdexcept>
-#include <utility>
-#include <vector>
 
-std::array<std::string, 8> cAnimationHeaders =
+namespace Animation
 {
-	"spritesheet-source",
-	"animation"
-};
-
-enum class AnimationHeader
-{
-	Unknown = -1,
-	SpritesheetSource,
-	Animation
-};
-
-AnimationHeader GetHeader(std::string headerstr)
-{
-	std::transform(headerstr.begin(), headerstr.end(), headerstr.begin(), ::tolower);
-	for(size_t i = 0; i < cAnimationHeaders.size(); ++i)
+	void Sprite::UpdateTextureRect()
 	{
-		if(cAnimationHeaders[i].compare(headerstr) == 0)
+		sf::IntRect textureRect = baseFrame;
+		if(isFlippedHorizontally)
 		{
-			return static_cast<AnimationHeader>(i);
+			textureRect.left += textureRect.width;
+			textureRect.width *= -1;
 		}
-	}
-
-	return AnimationHeader::Unknown;
-}
-
-std::array<std::string, 9> cAnimationProperties = 
-{
-	"frame-start-x",
-	"frame-start-y",
-	"frame-width",
-	"frame-height",
-	"frame-margin",
-	"frame-count",
-	"frame-time",
-	"reverse",
-	"loop"
-};
-
-enum class AnimationProperty
-{
-	Unknown = -1,
-	FrameStartX,
-	FrameStartY,
-	FrameWidth,
-	FrameHeight,
-	FrameMargin,
-	FrameCount,
-	FrameTime,
-	Reverse,
-	Loop
-};
-
-AnimationProperty GetProperty(std::string propertystr)
-{
-	std::transform(propertystr.begin(), propertystr.end(), propertystr.begin(), ::tolower);
-	for(size_t i = 0; i < cAnimationProperties.size(); ++i)
-	{
-		if(cAnimationProperties[i].compare(propertystr) == 0)
+		if(isFlippedVertically)
 		{
-			return static_cast<AnimationProperty>(i);
+			textureRect.top += textureRect.height;
+			textureRect.height *= -1;
 		}
+		setTextureRect(textureRect);
 	}
 
-	return AnimationProperty::Unknown;
-}
-
-Animation::Animation()
-	:frameStart(0U, 0U),
-	frameSize(0U, 0U),
-	frameMargin(0U),
-	frameCount(0U),
-	frameTime(0U),
-	reverse(false),
-	loop(false)
-{
-}
-
-void AnimatedSprite::UpdateTextureRect()
-{
-	auto textureRect = baseFrame;
-	if(isFlippedHorizontally)
+	void Sprite::GetFrame()
 	{
-		textureRect.left += textureRect.width;
-		textureRect.width *= -1;
-	}
-	if(isFlippedVertically)
-	{
-		textureRect.top += textureRect.height;
-		textureRect.height *= -1;
+		assert(currentAnimation != nullptr);
+		
+		if(currentAnimation->reverse)
+		{
+			baseFrame = sheet.GetFrame(*currentAnimation, currentAnimation->frameCount - (currentFrame + 1U));
+		}
+		else
+		{
+			baseFrame = sheet.GetFrame(*currentAnimation, currentFrame);
+		}
+		UpdateTextureRect();
 	}
 
-	setTextureRect(textureRect);
-}
-
-void AnimatedSprite::GetNextFrame()
-{
-	assert(currentAnimation != nullptr);
-
-	if(currentAnimation->frameCount < 2)
-		return;
-
-	++currentFrame;
-	if(currentFrame >= currentAnimation->frameCount)
+	bool Sprite::IsAnimationFinished() const
 	{
-		if(!currentAnimation->loop)
+		return currentFrame >= currentAnimation->frameCount;
+	}
+
+	void Sprite::FlipHorizontally()
+	{
+		isFlippedHorizontally = !isFlippedHorizontally;
+	}
+
+	void Sprite::FlipVertically()
+	{
+		isFlippedVertically = !isFlippedVertically;
+	}
+
+	bool Sprite::SetAnimation(const std::string & name)
+	{
+		auto * candidate = sheet.GetAnimation(name);
+		if(candidate != nullptr)
+		{
+			currentFrameTime = 0U;
+			currentFrame = 0U;
+			currentAnimation = candidate;
+			GetFrame();
+			return true;
+		}
+
+		return false;
+	}
+
+	void Sprite::Update(const sf::Time & elapsed)
+	{
+		assert(currentAnimation != nullptr);
+
+		if(IsAnimationFinished())
+		{
+			return;
+		}
+
+		currentFrameTime += elapsed.asMilliseconds();
+		if(currentFrameTime < currentAnimation->frameTime)
+		{
+			return;
+		}
+
+		currentFrameTime = 0U;
+		++currentFrame;
+		if(currentFrame < currentAnimation->frameCount)
+		{
+			GetFrame();
+		}
+		else if(currentAnimation->loop)
+		{
+			currentFrame = 0U;
+			GetFrame();
+		}
+		else
 		{
 			currentFrame = currentAnimation->frameCount;
 		}
-		else
-		{
-			currentFrame = 0;
-			baseFrame = sf::IntRect(
-				currentAnimation->frameStart.x,
-				currentAnimation->frameStart.y,
-				currentAnimation->frameSize.x,
-				currentAnimation->frameSize.y);
-
-			UpdateTextureRect();
-		}
-	}
-	else
-	{
-		if(currentAnimation->reverse)
-		{
-			baseFrame.left -= currentAnimation->frameMargin + currentAnimation->frameSize.x;
-		}
-		else
-		{
-			baseFrame.left += currentAnimation->frameMargin + currentAnimation->frameSize.x;
-		}
-
-		UpdateTextureRect();
-	}
-}
-
-std::vector<std::string> AnimatedSprite::GetAnimations() const
-{
-	std::vector<std::string> result;
-	for(auto iter = animations.begin(); iter != animations.end(); ++iter)
-	{
-		result.push_back(iter->first);
 	}
 
-	return result;
-}
-
-bool AnimatedSprite::AddAnimation(const Animation animation, const std::string & name)
-{
-	assert(name.size() > 0U);
-
-	if(animations.find(name) != animations.end())
+	Sprite::Sprite(const Sheet & sheetRef)
+		: sf::Sprite(sheetRef.GetTexture()),
+		sheet(sheetRef),
+		currentAnimation(nullptr),
+		baseFrame(),
+		currentFrame(0U),
+		currentFrameTime(0U),
+		isFlippedHorizontally(false),
+		isFlippedVertically(false)
 	{
-		return false;
-	}
-
-	animations[name] = animation;
-	return true;
-}
-
-void AnimatedSprite::ForceAddAnimation(const Animation animation, const std::string & name)
-{
-	assert(name.size() > 0U);
-
-	animations[name] = animation;
-}
-
-bool AnimatedSprite::SetAnimation(const std::string & name)
-{
-	auto findresult = animations.find(name);
-	if(findresult == animations.end())
-	{
-		return false;
-	}
-
-	currentAnimation = &(findresult->second);
-
-	baseFrame = sf::IntRect(
-		currentAnimation->frameStart.x,
-		currentAnimation->frameStart.y,
-		currentAnimation->frameSize.x,
-		currentAnimation->frameSize.y);
-	UpdateTextureRect();
-
-	currentFrame = 0U;
-	currentFrameTime = 0U;
-
-	return true;
-}
-
-bool AnimatedSprite::RemoveAnimation(const std::string & name)
-{
-	auto findresult = animations.find(name);
-	if(findresult != animations.end())
-	{
-		if(currentAnimation == &(findresult->second))
-		{
-			currentAnimation = nullptr;
-		}
-		animations.erase(findresult);
-		return true;
-	}
-
-	return false;
-}
-
-bool AnimatedSprite::IsAnimationFinished() const
-{
-	return (currentFrame >= currentAnimation->frameCount) && !currentAnimation->loop;
-}
-
-void AnimatedSprite::FlipHorizontally()
-{
-	isFlippedHorizontally = !isFlippedHorizontally;
-	UpdateTextureRect();
-}
-
-void AnimatedSprite::FlipVertically()
-{
-	isFlippedVertically = !isFlippedVertically;
-	UpdateTextureRect();
-}
-
-sf::Vector2f AnimatedSprite::GetSize() const
-{
-	auto & scale = getScale();
-	return sf::Vector2f(baseFrame.width * scale.x, baseFrame.height * scale.y);
-}
-
-const Animation & AnimatedSprite::GetCurrentAnimation() const
-{
-	return *currentAnimation;
-}
-
-Animation * AnimatedSprite::GetcurrentAnimationPtr()
-{
-	return const_cast<Animation *>(currentAnimation);
-}
-
-unsigned int AnimatedSprite::GetCurrentFrame() const
-{
-	return currentFrame;
-}
-
-unsigned int AnimatedSprite::GetCurrentFrameTime() const
-{
-	return currentFrameTime;
-}
-
-void AnimatedSprite::SetCurrentFrame(const unsigned int frameNo)
-{
-	currentFrame = frameNo;
-}
-
-Animation LoadAnimation(const std::vector<std::string> & definition, size_t & i)
-{
-	Animation result;
-	while(i < definition.size() && definition[i][0] == '\t')
-	{
-		std::stringstream ss;
-		ss.str(definition[i]);
-		
-		std::string key;
-		ss >> key;
-		const auto property = GetProperty(key);
-
-		switch(property)
-		{
-			case AnimationProperty::FrameStartX:
-			ss >> result.frameStart.x;
-			break;
-			
-			case AnimationProperty::FrameStartY:
-			ss >> result.frameStart.y;
-			break;
-
-			case AnimationProperty::FrameWidth:
-			ss >> result.frameSize.x;
-			break;
-
-			case AnimationProperty::FrameHeight:
-			ss >> result.frameSize.y;
-			break;
-
-			case AnimationProperty::FrameMargin:
-			ss >> result.frameMargin;
-			break;
-
-			case AnimationProperty::FrameCount:
-			ss >> result.frameCount;
-			break;
-
-			case AnimationProperty::FrameTime:
-			ss >> result.frameTime;
-			break;
-
-			case AnimationProperty::Reverse:
-			result.reverse = true;
-			break;
-
-			case AnimationProperty::Loop:
-			result.loop = true;
-			break;
-
-			default:
-			break;
-		}
-
-		++i;
-	}
-
-	return result;
-}
-
-void AnimatedSprite::LoadFromFile(const std::string & animationFile, sf::Texture & textureStorage)
-{
-	auto lines = Platform::LoadTextFile(animationFile, true, true);
-
-	for(size_t i = 0U; i < lines.size(); ++i)
-	{
-		std::stringstream ss;
-		ss.str(lines[i]);
-
-		std::string key;
-		ss >> key;
-
-		const auto header = GetHeader(key);
-		switch(header)
-		{
-			case AnimationHeader::SpritesheetSource:
-			{
-				const auto sourcefile = Util::GetStringInQuotes(lines[i]);
-				if(!textureStorage.loadFromFile(sourcefile))
-				{
-					throw std::runtime_error("Failed to load texture <" + sourcefile + "> specified in animation file <" + animationFile + ">.");
-				}
-				setTexture(textureStorage);
-			}
-			break;
-
-			case AnimationHeader::Animation:
-			{
-				std::string value;
-				ss >> value;
-
-				++i;
-				AddAnimation(LoadAnimation(lines, i), value);
-				--i; // decrement once because next loop we should consider that position
-			}
-			break;
-
-			default:
-			break;
-		}
-	}
-}
-
-void AnimatedSprite::Update(const sf::Time & elapsed)
-{
-	if(currentAnimation == nullptr)
-		return;
-
-	currentFrameTime += elapsed.asMilliseconds();
-	if(currentFrameTime >= currentAnimation->frameTime)
-	{
-		currentFrameTime = 0U;
-		GetNextFrame();
-	}
-}
-
-AnimatedSprite & AnimatedSprite::operator=(const AnimatedSprite & other)
-{
-	Sprite::operator=(other);
-	animations = other.animations;
-	currentAnimation = nullptr;
-	currentFrame = other.currentFrame;
-	currentFrameTime = other.currentFrameTime;
-	baseFrame = other.baseFrame;
-	isFlippedHorizontally = other.isFlippedHorizontally;
-	isFlippedVertically = other.isFlippedVertically;
-
-	if(other.currentAnimation != nullptr)
-	{
-		for(auto iter = other.animations.begin(); iter != other.animations.end(); ++iter)
-		{
-			if(&(iter->second) == other.currentAnimation)
-			{
-				SetAnimation(iter->first);
-				break;
-			}
-		}
-	}
-
-	return *this;
-}
-
-AnimatedSprite::AnimatedSprite()
-	: Sprite(),
-	animations(),
-	currentAnimation(nullptr),
-	currentFrame(0U),
-	currentFrameTime(0U),
-	baseFrame(),
-	isFlippedHorizontally(false),
-	isFlippedVertically(false)
-{
-}
-
-AnimatedSprite::AnimatedSprite(const AnimatedSprite & other)
-	: Sprite(other),
-	animations(other.animations),
-	currentAnimation(nullptr),
-	currentFrame(other.currentFrame),
-	currentFrameTime(other.currentFrameTime),
-	baseFrame(other.baseFrame),
-	isFlippedHorizontally(other.isFlippedHorizontally),
-	isFlippedVertically(other.isFlippedVertically)
-{
-	if(other.currentAnimation != nullptr)
-	{
-		for(auto iter = other.animations.begin(); iter != other.animations.end(); ++iter)
-		{
-			if(&(iter->second) == other.currentAnimation)
-			{
-				SetAnimation(iter->first);
-				break;
-			}
-		}
 	}
 }
--- smart_tales_ii/animatedsprite.hpp
+++ smart_tales_ii/animatedsprite.hpp
@@ -1,69 +1,34 @@
 #pragma once
-#include <SFML\Graphics.hpp>
-#include <SFML\System\Clock.hpp>
-#include <string>
-#include <unordered_map>
+#include "animationsheet.hpp"
 
-struct Animation
+namespace Animation
 {
-	sf::Vector2u frameStart;	// offset in pixels
-	sf::Vector2u frameSize;		// in pixels
-	unsigned int frameMargin;	// in pixels
-	unsigned int frameCount;
-	unsigned int frameTime;		// in milliseconds
-	bool reverse;
-	bool loop;
+	class Sprite : public sf::Sprite
+	{
+	private:
+		const Sheet & sheet;
+		
+		const Animation * currentAnimation;
+		sf::IntRect baseFrame;
+		unsigned int currentFrame;
+		unsigned int currentFrameTime;
+		bool isFlippedHorizontally;
+		bool isFlippedVertically;
 
-	Animation();
-};
+		void UpdateTextureRect();
+		void GetFrame();
 
-class AnimatedSprite : public sf::Sprite
-{
-private:
-	std::unordered_map<std::string, Animation> animations;
-	const Animation * currentAnimation;
-	unsigned int currentFrame;
-	unsigned int currentFrameTime;
-	// unflipped variant
-	sf::IntRect baseFrame;
-	bool isFlippedHorizontally;
-	bool isFlippedVertically;
+	public:
+		virtual bool IsAnimationFinished() const;
 
-protected:
-	virtual void UpdateTextureRect();
-	virtual void GetNextFrame();
+		virtual void FlipHorizontally();
+		virtual void FlipVertically();
+		
+		bool SetAnimation(const std::string & name);
 
-public:
-	virtual std::vector<std::string> GetAnimations() const;
+		virtual void Update(const sf::Time & elapsed);
 
-	// Returns false if animation already exists
-	virtual bool AddAnimation(const Animation animation, const std::string & name);
-	// May overwrite an exisiting entry
-	virtual void ForceAddAnimation(const Animation animation, const std::string & name);
-	virtual bool SetAnimation(const std::string & name);
-	virtual bool RemoveAnimation(const std::string & name);
-
-	virtual bool IsAnimationFinished() const;
-
-	virtual void FlipHorizontally();
-	virtual void FlipVertically();
-
-	// texture rectangle dimensions multiplied by scale
-	virtual sf::Vector2f GetSize() const;
-	virtual const Animation & GetCurrentAnimation() const;
-	// be careful with its internal state... Modifications made will be persistent
-	virtual Animation * GetcurrentAnimationPtr();
-	virtual unsigned int GetCurrentFrame() const;
-	virtual unsigned int GetCurrentFrameTime() const;
-
-	virtual void SetCurrentFrame(const unsigned int frameNo);
-
-	// May throw a runtime_error if the file or the file definition is wrong
-	virtual void LoadFromFile(const std::string & animationFile, sf::Texture & textureStorage);
-	virtual void Update(const sf::Time & elapsed);
-
-	AnimatedSprite & operator = (const AnimatedSprite & other);
-
-	AnimatedSprite();
-	AnimatedSprite(const AnimatedSprite & other);
-};
+		// Remember to set the animation afterwards!
+		Sprite(const Sheet & sheetRef);
+	};
+}
