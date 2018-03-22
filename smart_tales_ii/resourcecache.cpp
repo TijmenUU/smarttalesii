a2c43f32d64a294e2f4e512ae190b53e589bf539
Tijmen <tijmen_222@hotmail.com>
2018-03-22 19:07:19 +0100

    Singleton conversion of ResourceCache

--- smart_tales_ii/resourcecache.cpp
+++ smart_tales_ii/resourcecache.cpp
@@ -1,21 +1,48 @@
 #include "resourcecache.hpp"
 
-bool ResourceCache::LoadFont(const std::string & file, const std::string & name)
+#include "stringutil.hpp"
+
+ResourceCache & ResourceCache::GetInstance()
 {
+	ResourceCache singleton;
+	return singleton;
+}
+
+void ResourceCache::LoadFont(const std::string & file)
+{
+	const std::string name = Util::GetFileName(file);
+
 	fontStorage[name] = sf::Font();
 	if(!fontStorage[name].loadFromFile(file))
 	{
-		fontStorage.erase(name);
-		return false;
+		throw std::runtime_error("Error loading font <" + file + ">");
 	}
+}
 
-	return true;
+void ResourceCache::LoadTexture(const std::string & file)
+{
+	const std::string name = Util::GetFileName(file);
+	textureStorage[name] = sf::Texture();
+	if(!textureStorage[name].loadFromFile(file))
+	{
+		throw std::runtime_error("Error loading texture <" + file + ">");
+	}
 }
 
 sf::Font * ResourceCache::GetFont(const std::string & name)
 {
-	if(fontStorage.find(name) == fontStorage.end())
+	auto searchResult = fontStorage.find(name);
+	if(searchResult == fontStorage.end())
 		return nullptr;
 
-	return &(fontStorage[name]);
+	return &(searchResult->second);
+}
+
+sf::Texture * ResourceCache::GetTexture(const std::string & name)
+{
+	auto searchResult = textureStorage.find(name);
+	if(searchResult == textureStorage.end())
+		return nullptr;
+
+	return &(searchResult->second);
 }
--- smart_tales_ii/resourcecache.hpp
+++ smart_tales_ii/resourcecache.hpp
@@ -4,13 +4,30 @@
 #include <unordered_map>
 #include <vector>
 
-struct ResourceCache
+class ResourceCache
 {
 private:
 	std::unordered_map<std::string, sf::Font> fontStorage;
+	std::unordered_map<std::string, sf::Texture> textureStorage;
+
+protected:
+	friend class Program;
+	// Throws an runtime_error if the file cannot be loaded
+	// Uses the filename without extension as name
+	void LoadFont(const std::string & file);
+	// Throws an runtime_error if the file cannot be loaded
+	// Uses the filename without extension as name
+	void LoadTexture(const std::string & file);
 
 public:
-	bool LoadFont(const std::string & file, const std::string & name);
+	static ResourceCache & GetInstance();
 
 	sf::Font * GetFont(const std::string & name);
+	sf::Texture * GetTexture(const std::string & name);
+
+	ResourceCache() = default;
+	ResourceCache(const ResourceCache &) = delete;
+	ResourceCache(const ResourceCache &&) = delete;
+	ResourceCache & operator = (ResourceCache &) = delete;
+	ResourceCache & operator = (ResourceCache &&) = delete;
 };
