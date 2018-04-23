feac25824840740107a368b62ee24b0484d5d4c8
Tijmen <tijmen_222@hotmail.com>
2018-04-23 10:42:28 +0200

    ScoreOverlay message update

--- smart_tales_ii/scoreoverlay.cpp
+++ smart_tales_ii/scoreoverlay.cpp
@@ -45,10 +45,10 @@ void ScoreOverlay::Setup()
 	const float horizontalCenterLine = 800.f;
 
 	gameOverText.setFillColor(sf::Color::White);
-	gameOverText.setOutlineColor(sf::Color::Black);
-	gameOverText.setOutlineThickness(2.f);
+	//gameOverText.setOutlineColor(sf::Color::Black);
+	//gameOverText.setOutlineThickness(2.f);
 	const auto textBounds = gameOverText.getLocalBounds();
-	gameOverText.setPosition(Alignment::GetCenterOffset(textBounds.width, horizontalCenterLine) + textBounds.left, 175.f);
+	gameOverText.setPosition(Alignment::GetCenterOffset(textBounds.width, horizontalCenterLine) + textBounds.left, 100.f);
 
 	earnedCurrency.SetValue(0U);
 	earnedCurrency.CenterOn(horizontalCenterLine, 300.f);
@@ -87,13 +87,33 @@ void ScoreOverlay::Update(const sf::Time & elapsed, const Inputhandler & input)
 	}
 }
 
+// helper function
+std::string GetGameOverMsg(const Obstacle::Type cause)
+{
+	const static std::array<std::string, 5> messages = {
+		"Small accidents are scary!\n96 000 elderly had to visit\nthe emergency after one.", // Cijferrapportage Valongevallen 65 jaar en ouder, 2016
+		"Ouch! Tripping over accounts\nfor 23% of the elderly visiting\nthe emergency room.", // Cijferrapportage Valongevallen 65 jaar en ouder, 2016
+		"Practice makes perfect!\nNobody likes sitting still at\nhome.", // AAL Forum 2016: elderly want to have an active part in society
+		"Tech up! Simple aids\nare here to help.", // The elderly are afraid of using IT systems, KLEINBERGER et al 2007
+		"Big brother is watching?\nNot really, most simple sensors\noperate on their own!" // Privacy, Identity and Security in Ambient Intelligence, Friedewald et al 2005
+	};
+	static unsigned int idx = 0U;
+
+	if(idx >= messages.size())
+	{
+		idx = 0U;
+	}
+
+	return messages[idx++];
+}
+
 ScoreOverlay::ScoreOverlay(const Player::Score & score, 
 	const Player::Inventory & inventory, 
 	const Obstacle::Type gameOverCause)
 	:	playerScore(score),
 	playerInventory(inventory),
 	backgroundSprite(ResourceCache::GetInstance().GetSpriteSheet("scorebackground")),
-	gameOverText("" /* TODO convert cause to txt */, ResourceCache::GetInstance().GetFont("commodore"), 36),
+	gameOverText(GetGameOverMsg(gameOverCause), ResourceCache::GetInstance().GetFont("commodore"), 36),
 	earnedCurrency(5U, "Currency earned: ", score.GetTotalCurrency(), true),
 	balanceCurrency(5U, "Balance: ", inventory.GetCurrency(), true),
 	gotoShopButton(ResourceCache::GetInstance().GetSpriteSheet("navigationbutton_large"))
