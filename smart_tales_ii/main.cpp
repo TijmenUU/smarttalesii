8b3778b56d94ea71cf2793e59e5be6ccc2bcf55f
Tijmen van Nesselrooij <tijmen_222@hotmail.com>
2018-05-11 10:22:09 +0200

    Added nosound launch parameter support

--- smart_tales_ii/main.cpp
+++ smart_tales_ii/main.cpp
@@ -1,18 +1,61 @@
 #include "program.hpp"
+#include "soundmanager.hpp"
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 
 const unsigned int windowWidth = 1280U;
 const unsigned int windowHeight = 720U;
 
+enum class LaunchParameter
+{
+	Unknown = -1,
+	Debug,
+	NoSound
+};
+
+LaunchParameter GetLaunchParameterToken(const std::string & s)
+{
+	const static std::array<std::string, 2> values = {"debug", "nosound"};
+	for(size_t i = 0; i < values.size(); ++i)
+	{
+		if(values[i].compare(s) == 0)
+		{
+			return static_cast<LaunchParameter>(i);
+		}
+	}
+
+	return LaunchParameter::Unknown;
+}
+
 int main(int argc, char ** argv)
 {
 	bool debug = false;
-	if(argc >= 2 && std::string(argv[1]).compare("debug") == 0)
+
+	for(size_t i = 1; i < argc; ++i)
 	{
-		debug = true;
+		const auto result = GetLaunchParameterToken(argv[i]);
+		switch(result)
+		{
+			case LaunchParameter::Debug:
+			debug = true;
+			break;
+
+			case LaunchParameter::NoSound:
+			{
+				auto & instance = SoundManager::GetInstance();
+				instance.SetMusicMute(true);
+				instance.SetSFXMute(true);
+			}
+			break;
+
+			default:
+			case LaunchParameter::Unknown:
+			break;
+		}
 	}
+
 	Program * p;
 	try
 	{
